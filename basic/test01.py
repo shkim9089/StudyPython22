b865d56f7b65f6b497bb71650b892973ce57a8e7
shkim9089 <shkim3953@naver.com>
2022-06-03 16:13:42 +0900

    22년 06월03일 예제로 전체복습

--- basic/test01.py
+++ basic/test01.py
@@ -1,2 +1,13 @@
+from numpy import ascontiguousarray
+
+
 print("hello,python")
 print("저는 성명건입니다")
+
+
+asia={'일본','중국','한국'}
+asia.add('베트남')
+asia.add('중국')
+asia.remove('일본')
+asia.update({'미국'})
+print(asia)
